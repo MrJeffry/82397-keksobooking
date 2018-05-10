84143f81aef9ef2ee779ba44c78ba0bcb2ac7d13
Artem Altigin <aaltigin@gmail.com>
2018-05-10 12:04:11 +0300

    Реализовал загрузку фото

--- js/upload.js
+++ js/upload.js
@@ -8,19 +8,19 @@
     'PNG': ''
   };
 
-  var avatarImgContainer = document.querySelector('.ad-form-header__preview');
-  var uploadavatarImgContainer = document.querySelector('.ad-form-header__input');
-
+  var avatarImgContainer = window.util.adForm.querySelector('.ad-form-header__preview');
+  var uploadAvatarImgContainer = window.util.adForm.querySelector('.ad-form-header__input');
   var uploadAdImageControl = window.util.adForm.querySelector('.ad-form__input');
-  var previewadImageContainer = window.util.adForm.querySelector('ad-form__photo');
+  var previewAdContainer = window.util.adForm.querySelector('.ad-form__photo');
+  var previewAdImageContainer = window.util.adForm.querySelector('.ad-form__photo-container');
 
-  var onChangeInputFiles = function () {
-    for (var i = 0; i < this.files.length; i++) {
-      showPreviewImage(this.files[i]);
+  var onChangeInputFiles = function (evt) {
+    for (var i = 0; i < evt.target.files.length; i++) {
+      showPreviewImage(evt.target.files[i], evt.target);
     }
   };
 
-  var showPreviewImage = function (imageFile) {
+  var showPreviewImage = function (imageFile, elem) {
     var fileRegExp = new RegExp('^image/(' + Object.keys(TYPES_OF_IMAGES).join('|').replace('\+', '\\+') + ')$', 'i');
 
     if (!fileRegExp.test(imageFile.type)) {
@@ -28,18 +28,31 @@
     }
 
     var fileReader = new FileReader();
-    fileReader.addEventListener('load', displayImageViaFileRader);
-    fileReader.readAsDataURL(imageFile);
+    if (elem.classList.contains('ad-form-header__input')) {
+      fileReader.addEventListener('load', displayImageViaFileRaderAvatar);
+      fileReader.readAsDataURL(imageFile);
+    } else {
+      fileReader.addEventListener('load', displayImageViaFileRaderAddImg);
+      fileReader.readAsDataURL(imageFile);
+    }
   };
 
-  var displayImageViaFileRader = function (evt) {
+  var displayImageViaFileRaderAddImg = function (evt) {
+    var clone = previewAdContainer.cloneNode();
     var uploadImage = document.createElement('img');
-    window.util.deleteAllElements(avatarImgContainer, 'img');
-    avatarImgContainer.appendChild(uploadImage);
+    uploadImage.style.width = 100 + '%';
+    uploadImage.style.height = 100 + '%';
+    clone.appendChild(uploadImage);
+    previewAdImageContainer.appendChild(clone);
     uploadImage.src = evt.target.result;
   };
 
-  uploadavatarImgContainer.addEventListener('change', onChangeInputFiles);
+  var displayImageViaFileRaderAvatar = function (evt) {
+    var uploadImage = avatarImgContainer.querySelector('img');
+    uploadImage.src = evt.target.result;
+  };
+
+  uploadAvatarImgContainer.addEventListener('change', onChangeInputFiles);
   uploadAdImageControl.addEventListener('change', onChangeInputFiles);
 
 })();
